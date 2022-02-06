bb85b7370e172d910a84df5e6a66a776b85a0d90
ivannofick\windows <ivannofick>
2022-02-06 17:41:44 +0700

    [add] test 2

--- main.go
+++ main.go
@@ -8,20 +8,24 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H {
-			"name" : "ivannofick",
-			"bio" : "A Profesional Developer",
-		})
-	})
+	router.GET("/", rootHandler)
 
-	router.GET("/my/profile", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H {
-			"name" : "ivannofick",
-			"bio" : "A Profesional Developer",
-			"bod" : "18-xx-xxxx",
-		})
-	})
+	router.GET("/my/profile", myProfileHandler)
 
-	router.Run(":8100")
+	router.Run()
+}
+
+func rootHandler(c *gin.Context)  {
+	c.JSON(http.StatusOK, gin.H {
+		"name" : "ivannofick",
+		"bio" : "A Profesional Developer",
+	})
+}
+
+func myProfileHandler(c *gin.Context)  {
+	c.JSON(http.StatusOK, gin.H {
+		"name" : "ivannofick",
+		"bio" : "A Profesional Developer",
+		"bod" : "18-xx-xxxx",
+	})
 }
